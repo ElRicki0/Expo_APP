021cd708d1fad44b6e1ffadd9354d80ef011c9fc
Jhonnyy0 <136754303+Jhonnyy0@users.noreply.github.com>
2024-06-21 20:24:02 -0600

    Diseño de login

--- App.js
+++ App.js
@@ -1,20 +1,113 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, useWindowDimensions } from 'react-native';
+import { TextInput, Button, Image } from 'react-native';
+import * as Font from 'expo-font';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!fontsLoaded) {
+            loadFonts();
+        }
+    });
+
+    const loadFonts = async () => {
+        await Font.loadAsync({
+            'Poppins-Thin': require('./src/Fonts/Poppins-Thin.ttf'),
+        });
+
+        setFontsLoaded(true);
+    }
+
+    if (!fontsLoaded) {
+        return(<View/>);
+    }
+    return (
+        <View style={styles.container}>
+            <Image source={require('./src/img/logo.png')} />
+            <Text style={styles.title}>QUIROPRACTICA ESPECÍFICA</Text>
+
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Correo Electronico"
+                    placeholderTextColor="white"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Contraseña"
+                    placeholderTextColor="white"
+                    secureTextEntry={true}
+                />
+            </View>
+
+            <TouchableOpacity style={styles.forgotPasswordContainer}>
+                <Text style={styles.forgotPassword}>
+                    Olvidaste tu contraseña?
+                </Text>
+            </TouchableOpacity>
+
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    title="Aceptar"
+                    color="#216608"
+                />
+            </View>
+
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+    },
+
+    title: {
+        fontSize: 22,
+        fontFamily: 'Poppins-Thin',
+        textAlign: 'center',
+        color: 'white',
+        marginBottom: 40,
+    },
+
+    inputContainer: {
+        width: '80%',
+    },
+
+    input: {
+        height: 40,
+        borderRadius: 40,
+        backgroundColor: '#216608',
+        color: 'white',
+        paddingHorizontal: 25,
+        marginBottom: 15,
+        height: 50,
+    },
+
+    forgotPasswordContainer: {
+        width: '80%',
+        alignSelf: 'flex-start',
+    },
+
+    forgotPassword: {
+        color: 'white',
+        textAlign: 'left',
+        marginLeft: 50,
+    },
+
+    buttonContainer: {
+        marginTop: 40,
+        width: '40%',
+    },
+
+    button: {
+        color: 'white',
+    },
 });
